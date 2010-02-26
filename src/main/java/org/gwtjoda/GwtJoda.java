5c5840494e685c3bf8db98a241e26ada2ab5f746
organicveggie <organicveggie@gmail.com>
2010-02-26 20:48:52 +0000

    Initial check in.

---
+++ src/main/java/org/gwtjoda/GwtJoda.java
@@ -0,0 +1,14 @@
+package org.gwtjoda;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GwtJoda implements EntryPoint {
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+    }
+}
